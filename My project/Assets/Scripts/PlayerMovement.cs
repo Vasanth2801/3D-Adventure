c11d1d2bb07cafdc6238dada897b16442af26fbc
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-10-24 08:39:18 +0530

    Terrain Setup

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -40,11 +40,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVecocity, smoothSpeed);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(direction * speed * Time.deltaTime); 
+            controller.Move(moveDir.normalized * speed * Time.deltaTime); 
         }
     }
 }
